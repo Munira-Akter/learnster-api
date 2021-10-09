53e902028336da648d5edf33de573a7068990a86
Davor Minchorov <davorminchorov@gmail.com>
2021-10-09 04:46:01 +0200

    Implement Laravel Sanctum and Laravel Fortify

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,13 +20,14 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->freeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
